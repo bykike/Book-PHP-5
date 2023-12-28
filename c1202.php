529f47c4cc16c42b28c3515d6b1d63bd49d0b07c
Kike Algarra <kike@areavisual.net>
2023-12-28 12:25:40 +0100

    c1202

---
+++ c1202.php
@@ -0,0 +1,55 @@
+<HTML>
+	<HEAD>
+		<TITLE>Contenido de tabla Contratos </TITLE>
+	</HEAD>
+	<BODY>
+		<CENTER> 
+		<H3>Tabla Contratos</H3><BR><B>
+			<TABLE BORDER="1">
+				<TR BGCOLOR="yellow">
+					<TD>ID. CTO.</TD>
+					<TD>NIF</TD>
+					<TD>Fecha apertura</TD>
+					<TD>Estado</TD>
+				</TR>
+				<TR>
+					<TD>44500</TD>
+					<TD>12345</TD>
+					<TD>12/02/2003</TD>
+					<TD>Activo</TD>
+				</TR>
+				<TR>
+					<TD>44501</TD>
+					<TD>12345</TD>
+					<TD>12/04/2003</TD>
+					<TD>Activo</TD>
+				</TR>
+				<TR>
+					<TD>44502</TD>
+					<TD>45678</TD>
+					<TD>22/04/2001</TD>
+					<TD>Cancelado</TD>
+				</TR>
+				<TR>
+					<TD>44503</TD>
+					<TD>65778</TD>
+					<TD>25/08/2001</TD>
+					<TD>Activo</TD>
+				</TR>
+				<TR>
+					<TD>44504</TD>
+					<TD>65778</TD>
+					<TD>30/03/2002</TD>
+					<TD>Activo</TD>
+				</TR>
+			</TABLE>	 	 
+		</CENTER>
+	</BODY>
+</HTML>
+
+	
+
+
+ 
+
+ 
