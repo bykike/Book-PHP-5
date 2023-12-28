057609f410f8e21a5c82d3542560c23cce0126fe
Kike Algarra <kike@areavisual.net>
2023-12-28 12:23:19 +0100

    c1201
    c1201.

---
+++ c1201.php
@@ -0,0 +1,49 @@
+<HTML>
+	<HEAD>
+		<TITLE>Contenido de tabla Personas </TITLE>
+	</HEAD>
+	<BODY>
+		<CENTER> 
+		<H3>Tabla Personas</H3><BR><B>
+			<TABLE BORDER="1">
+				<TR BGCOLOR="yellow">
+					<TD>NIF</TD>
+					<TD>Nombre</TD>
+					<TD>Dirección</TD>
+					<TD>Teléfono</TD>
+				</TR>
+				<TR>
+					<TD>1234</TD>
+					<TD>José Pérez</TD>
+					<TD>avda. Libertad 4, Madrid</TD>
+					<TD>91 2330045</TD>
+				</TR>
+				<TR>
+					<TD>12345</TD>
+					<TD>Juan Rodríguez</TD>
+					<TD>calle Montemar 5, Toledo</TD>
+					<TD>91 45660045</TD>
+				</TR>
+				<TR>
+					<TD>45678</TD>
+					<TD>Luís García</TD>
+					<TD>calle Constitución 67, Valencia</TD>
+					<TD>977 45660045</TD>
+				</TR>
+				<TR>
+					<TD>65778</TD>
+					<TD>Jaime Font</TD>
+					<TD>calle Italia 88, Gerona</TD>
+					<TD>975 45655042</TD>
+				</TR>
+			</TABLE>	 	 
+		</CENTER>
+	</BODY>
+</HTML>
+
+	
+
+
+ 
+
+ 
