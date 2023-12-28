4aae57ea3f59c9c3d030e32e1c94893c4e6a50e0
Kike Algarra <kike@areavisual.net>
2023-12-28 12:45:38 +0100

    c1203
    c1203.

---
+++ c1203.php
@@ -0,0 +1,61 @@
+<HTML>
+	<HEAD>
+		<TITLE>Contenido de tabla Movimientos </TITLE>
+	</HEAD>
+	<BODY>
+		<CENTER> 
+		<H3>Tabla Movimientos</H3><BR><B>
+			<TABLE BORDER="1">
+				<TR BGCOLOR="yellow">
+					<TD>ID. CTO.</TD>
+					<TD>Fecha movimiento</TD>
+					<TD>Tipo</TD>
+					<TD>Cantidad</TD>
+				</TR>
+				<TR>
+					<TD>44500</TD>
+					<TD>1/2/2004</TD>
+					<TD>Debe</TD>
+					<TD>2000</TD>
+				</TR>
+				<TR>
+					<TD>44501</TD>
+					<TD>2/2/2004</TD>
+					<TD>Haber</TD>
+					<TD>1550</TD>
+				</TR>
+				<TR>
+					<TD>44503</TD>
+					<TD>13/3/2004</TD>
+					<TD>Debe</TD>
+					<TD>8500</TD>
+				</TR>
+				<TR>
+					<TD>44503</TD>
+					<TD>15/4/2004</TD>
+					<TD>Debe</TD>
+					<TD>6070</TD>
+				</TR>
+				<TR>
+					<TD>44503</TD>
+					<TD>16/4/2004</TD>
+					<TD>Haber</TD>
+					<TD>12090</TD>
+				</TR>
+				<TR>
+					<TD>44504</TD>
+					<TD>1/4/2004</TD>
+					<TD>Debe</TD>
+					<TD>9500</TD>
+				</TR>
+			</TABLE>	 	 
+		</CENTER>
+	</BODY>
+</HTML>
+
+	
+
+
+ 
+
+ 
